ad07a8f68fdb6799e2d35594cdda702534a3c587
rajumuduguri <raju.techm1990@.com>
2025-08-28 09:45:31 +0530

    added my sample code chaneges

---
+++ src/main/java/ebazar/com/EbazarAppApplication.java
@@ -0,0 +1,15 @@
+package ebazar.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbazarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbazarAppApplication.class, args);
+
+		System.out.println("Events Bazaar Application Started..");
+	}
+
+}
---
+++ src/test/java/ebazar/com/EbazarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ebazar.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbazarAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
